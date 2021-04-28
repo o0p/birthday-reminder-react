eb22bbe241cb7fa4758d6b4d0be9b4e66a12bd00
Ruslan <rus@gmx.com>
2021-04-28 23:59:55 +0100

    Add people list to App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <main>
       <section className="container">
         <h3>0 birthdays today</h3>
-        <List />
+        <List people={people} />
         <button onClick={() => console.log('cicked')}>clear all</button>
       </section>
     </main>
